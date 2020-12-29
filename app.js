e13fa76e7e02681a0924bd03b089f5479203033e
EldasDEV | Shavkat Toshpulotov <73429646+eldasdev@users.noreply.github.com>
2020-12-29 15:10:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const countDate = new Date('jan 1, 2021, 00:00:00').getTime();
+
+function newYear() {
+    const now = new Date().getTime();
+    gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap / (day));
+    const h = Math.floor((gap % (day)) / (hour));
+    const m = Math.floor((gap % (hour)) / (minute));
+    const s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    newYear();
+},1000)
